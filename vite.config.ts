2621eb53c37a6231f6db98814cf4a08f72ae1f93
alamsyahh15 <black.taurus001@gmail.com>
2025-06-16 14:23:14 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: '103.181.143.153',
+    port: 3000,
+  },
+  preview: {
+    host: '103.181.143.153',
+    port: 3000,
   },
 });
